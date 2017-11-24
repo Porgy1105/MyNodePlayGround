0e36cec297194c820b522d932cfe9f1fce1e348c
Porgy1105 <rmiyazaki1105@gmail.com>
2017-11-24 14:37:53 +0900

    京王線時刻表スクレイピング処理追加

--- src/my-puppeteer.js
+++ src/my-puppeteer.js
@@ -1,27 +1,43 @@
 const puppeteer = require('puppeteer');
 
-async function spa_access() {
-  const browser = await puppeteer.launch({ headless: false });
+// 開発時は unhandledRejection を subscribe する
+process.on('unhandledRejection', (e) => console.log(e))
+
+async function getKeiouTimeTable(url) {
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
-  await page.goto("http://keio.ekitan.com/pc/T5?dw=0&slCode=263-1&d=1");
 
-  const _tmp = await page.evaluate(() => {
-    const ele = document.getElementsByClassName("k_1301");
-    const array = [];
+  await page.goto(url);
 
-    for (let i = 0, l = ele.length; i < l; i++) {
-      array.push(ele[i].innerText);
+  const timeTable = await page.evaluate(() => {
+    let result = [];
+    let hourEle = document.getElementsByClassName("weekday");
+
+    for (let i = 0, l = hourEle.length; i < l; i++) {
+      let minitesTd = hourEle[i].nextElementSibling;
+      let minitesEleList = minitesTd.getElementsByTagName('span');
+      let timeTableObj = {};
+      let minitesList = [];
+
+      for (let j = 0, m = minitesEleList.length; j < m; j++) {
+        let minutes = minitesEleList[j].innerText.replace(/[^0-9]/g, '');
+        if (minutes) {
+          minitesList.push(minutes);
+        }
+      }
+
+      timeTableObj[hourEle[i].innerText] = minitesList;
+
+      result.push(timeTableObj);
     }
 
-    return array;
+    return result;
   });
 
-  console.log(_tmp);
+  console.log(timeTable);
 
   await browser.close();
 }
 
-// 開発時は unhandledRejection を subscribe する
-process.on('unhandledRejection', (e) => console.log(e))
-
-spa_access();
+// 新宿下り
+getKeiouTimeTable("http://keio.ekitan.com/pc/T5?dw=0&slCode=263-1&d=2");
