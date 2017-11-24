56e7bab8243e2fc0bd92009c53b7d30da0739525
Porgy1105 <rmiyazaki1105@gmail.com>
2017-11-24 17:13:20 +0900

    puppeteerスクライピング、JR時刻表取得処理追加

--- src/my-puppeteer.js
+++ src/my-puppeteer.js
@@ -14,10 +14,11 @@ async function getKeiouTimeTable(url) {
     let hourEle = document.getElementsByClassName("weekday");
 
     for (let i = 0, l = hourEle.length; i < l; i++) {
-      let minitesTd = hourEle[i].nextElementSibling;
-      let minitesEleList = minitesTd.getElementsByTagName('span');
       let timeTableObj = {};
       let minitesList = [];
+
+      let minitesTd = hourEle[i].nextElementSibling;
+      let minitesEleList = minitesTd.getElementsByTagName('span');
 
       for (let j = 0, m = minitesEleList.length; j < m; j++) {
         let minutes = minitesEleList[j].innerText.replace(/[^0-9]/g, '');
@@ -39,5 +40,70 @@ async function getKeiouTimeTable(url) {
   await browser.close();
 }
 
+async function getJRTimeTable(url) {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+
+  await page.goto(url);
+
+  const timeTable = await page.evaluate(() => {
+    let result = [];
+    let timeTableEle = document.getElementsByTagName("table")[0];
+    let timeTabletTrList = timeTableEle.querySelectorAll('tr:nth-child(n+3)');
+
+    for (let i = 0, l = timeTabletTrList.length; i < l; i++) {
+      let timeTableObj = {};
+      let minitesList = [];
+
+      let minitesTd = timeTabletTrList[i].lastElementChild;
+      let minitesEleList = minitesTd.querySelectorAll('div.timetable_time');
+
+      for (let j = 0, m = minitesEleList.length; j < m; j++) {
+        let minutesObj = {};
+
+        // 電車種別の取得
+        let trainText = minitesEleList[j].getAttribute('data-train');
+        if (trainText !== "無印") {
+          minutesObj.train = trainText;
+        } else {
+          minutesObj.train = "";
+        }
+
+        // 行き先の取得
+        let destinationText = minitesEleList[j].getAttribute('data-dest');
+        if (destinationText !== "無印") {
+          minutesObj.destination = destinationText;
+        } else {
+          minutesObj.destination = "";
+        }
+
+        // 時刻の取得
+        let minutesText = minitesEleList[j].querySelector("span.minute").innerText.replace(/[^0-9]/g, '');
+        if (minutesText) {
+          minutesObj.minutes = minutesText;
+          minitesList.push(minutesObj);
+        }
+      }
+
+      let hour = timeTabletTrList[i].firstElementChild.innerText;
+      timeTableObj[hour] = minitesList;
+
+      result.push(timeTableObj);
+    }
+
+    return result;
+  });
+
+  console.log(timeTable);
+
+  await browser.close();
+}
+
 // 新宿下り
-getKeiouTimeTable("http://keio.ekitan.com/pc/T5?dw=0&slCode=263-1&d=2");
+//getKeiouTimeTable("http://keio.ekitan.com/pc/T5?dw=0&slCode=263-1&d=2");
+
+// 川崎-東海道-平日-上り
+//getJRTimeTable("http://www.jreast-timetable.jp/1712/timetable/tt0526/0526020.html");
+
+// 川崎-南武線-平日-下り
+getJRTimeTable("http://www.jreast-timetable.jp/1712/timetable/tt0526/0526030.html");
